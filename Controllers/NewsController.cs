210defaf818d053831ac2aa019fb135b9b9c1c75
KhothatsoKay <khothatsoramotholo@gmail.com>
2023-06-01 11:43:52 +0200

    updated News

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -5,38 +5,44 @@ using Newtonsoft.Json;
 using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace Blog.Controllers
 {
     public class NewsController : Controller
-
     {
         private readonly HttpClient _httpClient;
-        private readonly string _apiKey = "63f0550476b54a4592813df9c6866b91";
+        private readonly string _apiKey = "317c324993b59179ea11ef42dfd5dd0e";
 
         public NewsController(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
-       
-      public async Task<IActionResult> Index()
-{
-    System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
-    var apiUrl = $"https://newsapi.org/v2/top-headlines?category=technology&apiKey={_apiKey}";
+        public async Task<IActionResult> Index()
+        {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            var apiUrl = $"https://gnews.io/api/v4/search?q=technology&token={_apiKey}";
 
-    var response = await _httpClient.GetAsync(apiUrl);
-    if (response.IsSuccessStatusCode)
-    {
-        var articlesResponse = JsonConvert.DeserializeObject<NewsApiResponse>(await response.Content.ReadAsStringAsync());
-        var articles = articlesResponse.Articles;
+            var response = await _httpClient.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var articlesResponse = JsonConvert.DeserializeObject<NewsApiResponse>(await response.Content.ReadAsStringAsync());
+                var articles = articlesResponse.Articles;
 
-        return View(articles);
+                // Filter articles that have content and image
+                var articlesWithContentAndImage = new List<NewsArticle>();
+                foreach (var article in articles)
+                {
+                    if (!string.IsNullOrWhiteSpace(article.Content) && !string.IsNullOrWhiteSpace(article.Image))
+                    {
+                        articlesWithContentAndImage.Add(article);
+                    }
+                }
+
+                return View(articlesWithContentAndImage);
+            }
+
+            // Handle the case when the response is not successful
+            return View(new List<NewsArticle>()); // or return an error view with an empty model
+        }
     }
-
-    // Handle the case when the response is not successful
-    return View(new List<NewsArticle>()); // or return an error view with an empty model
-}
-
-}
 }
--- Models/NewsApiResponse.cs
+++ Models/NewsApiResponse.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Blog.Models
@@ -6,11 +7,22 @@ namespace Blog.Models
     {
         public List<NewsArticle> Articles { get; set; }
     }
+    public class NewsSource
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
 
     public class NewsArticle
     {
         public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
         public string Url { get; set; }
-        public string PublishedAt { get; set; }
+        public string Image { get; set; } // Updated property name
+        public DateTime PublishedAt { get; set; }
+        public NewsSource Source { get; set; }
+
     }
 }
