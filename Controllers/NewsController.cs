574808c620dff31b5c32511e8b29e695f0112c53
KhothatsoKay <rammzz39@gmail.com>
2023-06-05 12:50:46 +0200

    made changes to news functionality

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -10,7 +10,7 @@ namespace Blog.Controllers
     public class NewsController : Controller
     {
         private readonly HttpClient _httpClient;
-        private readonly string _apiKey = "317c324993b59179ea11ef42dfd5dd0e";
+        private readonly string _apiKey = "";
 
         public NewsController(HttpClient httpClient)
         {
